3e54050b85326aa174cf99ddeeca45a4e588949a
kaisa316 <490788241@qq.com>
2020-11-09 20:47:36 +0800

    订制custom

--- config/config.default.php
+++ config/config.default.php
@@ -9,7 +9,7 @@ return array(
      * support extension: uprofiler, tideways_xhprof, tideways, xhprof
      * default: xhprof
      */
-    'extension' => 'xhprof',
+    'extension' => 'tideways_xhprof',
 
     // Can be either mongodb or file.
     /*
@@ -35,7 +35,18 @@ return array(
     // Profile 1 in 100 requests.
     // You can return true to profile every request.
     'profiler.enable' => function() {
-        return true;//rand(1, 100) === 42;
+		//return rand(1, 100) === 42;//system default
+		$url = isset($_SERVER['HTTP_HOST']) ? $_SERVER['HTTP_HOST'] : '';
+		if(strpos($_SERVER['REQUEST_URI'],'favicon.ico') !== false) {
+			return false;//icon文件不参与	
+		}
+		if(strpos($url,'xhprof') !== false) {
+			return false;//xhprof.com本身不参与
+		}
+		if (strpos($url, 'local') !== false) {
+			return true;
+		}
+		//return rand(1, 100) === 42;
     },
 
     'profiler.simple_url' => function($url) {
